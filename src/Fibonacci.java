a67e38d943d24d9fced288894a76e229ad07045a
DELL <shivamtiwari4393@gmail.com>
2026-05-09 14:06:28 +0530

    Commit

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,20 +1,15 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Fibonacci {
 
-    static int fib(int n) {
-        if (n <= 1)
-            return n;
-        return fib(n - 1) + fib(n - 2);
-    }
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter n: ");
-        int n = sc.nextInt();
-
-        System.out.println("Fibonacci number: " + fib(n));
-        sc.close();
-    }
+  public static void main(String[]args){
+      int a =0, b= 1, c , n =10;
+      for(int i =1; i<=n; i++){
+          System.out.println( a+" ");
+          c =a+b;
+          a=b ;
+          b=c;
+      }
+  }
 }
